4a321c303b89c61470f1bd4fa9691eac3d279927
weizeng <18550497663@sina.cn>
2018-12-05 15:22:35 +0800

    new porject demo

---
+++ src/main/java/com/example/xbcservicedemo/XbcServiceDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.xbcservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbcServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XbcServiceDemoApplication.class, args);
+    }
+}
